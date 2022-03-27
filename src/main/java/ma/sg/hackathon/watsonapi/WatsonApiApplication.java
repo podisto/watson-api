7c0f7a87c4f026f1e928cdb1c30c8de012f3c421
ElHadji Omar DIONE <omardione1991@gmail.com>
2022-03-27 21:00:33 +0000

    cors filter

--- src/main/java/ma/sg/hackathon/watsonapi/WatsonApiApplication.java
+++ src/main/java/ma/sg/hackathon/watsonapi/WatsonApiApplication.java
@@ -3,6 +3,10 @@ package ma.sg.hackathon.watsonapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("ma.sg.hackathon.watsonapi")
@@ -10,5 +14,24 @@ public class WatsonApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WatsonApiApplication.class, args);
+	}
+
+
+	@Bean
+	public CorsFilter corsFilter() {
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
 	}
 }
